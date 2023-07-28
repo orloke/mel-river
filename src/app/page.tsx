2b37fd1dc9f03fa1e54c4a97972f3dc742582663
Junior Dering <juniordering@hotmail.com>
2023-07-27 20:30:33 -0400

    feat: some global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-
 export default function Home() {
-  return <main>Olá mundo cruel</main>;
+  return (
+    <div>
+
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        phone: { max: "425px" },
+        tablet: { max: "768px" },
+        mdTablet: { max: "1080px" },
+        desktop: { max: "1366px" },
+        mdDesktop: { max: "1560px" },
+        lgDesktop: { max: "1920px" },
       },
     },
   },
   plugins: [],
-}
+};
