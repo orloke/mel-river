2cfa617b9b9cce1e80182f61071131be04155f50
Junior Dering <juniordering@hotmail.com>
2023-07-30 09:49:25 -0400

    feat: install gsap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ import { Banner } from "@/components/Banner";
 import { Cases } from "@/components/Cases";
 import { Header } from "@/components/Header";
 import { IntroOverlay } from "@/components/IntroOverlay";
+import { gsap } from "gsap";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    const tl = gsap.timeline()
+
+    tl.from
+
   }, []);
   return (
     <>
